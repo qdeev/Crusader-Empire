bcc0b6cc8412c4e2c3835ee2a893a67010902066
Artem822 <94913882+Artem822@users.noreply.github.com>
2022-01-28 14:40:02 +0300

    Add files via upload

--- modifiers.py
+++ modifiers.py
@@ -1,5 +1,3 @@
-import pygame.key
-
 from CONSTANTS import *
 
 
@@ -13,9 +11,54 @@ class Modifier:
 
 class Ability:
 
-    def __init__(self, bind_key, action):
+    def __init__(self, bind_key, action_type: ActionType):
         self.bind_key = bind_key
-        self.action = action
+        self.action_type = action_type
 
-    def cast(self):
-        pass
+    def cast(self, target, args: Dict):
+        if self.action_type == ActionType.NULL:
+            pass
+        elif self.action_type == ActionType.MOVE:
+            target.move(args["args"])
+        elif self.action_type == ActionType.RALLYPOINT:
+            target.set_rallypoint(args["args"])
+        elif self.action_type == ActionType.CREATE_WORKER:
+            target.create_task_worker()
+        elif self.action_type == ActionType.CREATE_SPEARMAN:
+            target.create_task_spearman()
+        elif self.action_type == ActionType.CREATE_SWORDSMAN:
+            target.create_task_swordsman()
+        elif self.action_type == ActionType.CREATE_FORTRESS:
+            target.create_task_fortress(args["args"])
+        elif self.action_type == ActionType.CREATE_BARACK:
+            target.create_task_barack(args["args"])
+        elif self.action_type == ActionType.CREATE_SUPPLY:
+            target.create_task_barack(args["args"])
+
+
+
+class AbilityHandler:
+
+    def __init__(self, unit, abilities: List[Ability]):
+        self.unit = unit
+        self.abilities = abilities
+
+    def get_ability(self, key):
+        for ability in self.abilities:
+            if ability.bind_key == key:
+                return ability
+        return NULL_ABILITY
+
+    def cast_ability(self, key, args: Dict):
+        self.get_ability(key).cast(self.unit, args)
+
+
+NULL_ABILITY = Ability(pygame.K_ESCAPE, ActionType.NULL)
+MOVE_ABILITY = Ability(pygame.BUTTON_RIGHT, ActionType.MOVE)
+RALLYPOINT_ABILITY = Ability(pygame.BUTTON_RIGHT, ActionType.RALLYPOINT)
+CREATE_WORKER_ABILITY = Ability(pygame.K_w, ActionType.CREATE_WORKER)
+CREATE_SWORDSMAN_ABILITY = Ability(pygame.K_s, ActionType.CREATE_SWORDSMAN)
+CREATE_SPEARMAN_ABILITY = Ability(pygame.K_d, ActionType.CREATE_SPEARMAN)
+CREATE_FORTRESS_ABILITY = Ability(pygame.K_f, ActionType.CREATE_FORTRESS)
+CREATE_BARACK_ABILITY = Ability(pygame.K_b, ActionType.CREATE_BARACK)
+CREATE_SUPPLY_ABILITY = Ability(pygame.K_v, ActionType.CREATE_SUPPLY)
---
+++ physics.py
@@ -0,0 +1,105 @@
+import pygame.sprite
+
+import unit_sprites
+import units
+from CONSTANTS import *
+import buildings
+from units import Unit
+
+
+class PhysicObject:
+
+    def __init__(self, game):
+        self.game = game
+
+
+class RigidBody(PhysicObject):
+
+    def __init__(self, game):
+        super().__init__(game)
+        self.draw = 0
+
+        self.velocity = pygame.math.Vector2()
+        self.velocity.x, self.velocity.y = 0, 0
+
+    def avoid(self):
+        for unit in filter(lambda x: isinstance(x, Unit), self.game.all_sprites_group):
+            if unit != self:
+                shift = pygame.math.Vector2(self.rect.center) - pygame.math.Vector2(unit.rect.center)
+                if pygame.Rect.colliderect(self.rect, unit.rect):
+                    self.rect.center += shift.normalize()
+
+    def collide(self):
+        hits_list = pygame.sprite.spritecollide(self, pygame.sprite.Group(list(filter(lambda x: not isinstance(x, Unit),
+                                                                                      self.game.all_sprites_group))),
+                                                False)
+        if hits_list:
+            if hits_list[0].rect.centerx > self.rect.centerx:
+                x = hits_list[0].rect.left - self.rect.width / 2
+            elif hits_list[0].rect.centerx < self.rect.centerx:
+                x = hits_list[0].rect.right + self.rect.width / 2
+            else:
+                x = self.rect.center[0]
+            self.velocity.x = 0
+            self.rect.centerx = x
+
+        hits_list = pygame.sprite.spritecollide(self, pygame.sprite.Group(list(filter(lambda x: not isinstance(x, Unit),
+                                                                                      self.game.all_sprites_group))),
+                                                False)
+        if hits_list:
+            if hits_list[0].rect.centery > self.rect.centery:
+                y = hits_list[0].rect.top - self.rect.height / 2
+            elif hits_list[0].rect.centery < self.rect.centery:
+                y = hits_list[0].rect.bottom + self.rect.height / 2
+            else:
+                y = self.rect.center[1]
+            self.velocity.y = 0
+            self.rect.centery = y
+
+    def process_physics(self, pos: Tuple[int, int]):
+        try:
+            leg_x, leg_y = abs(pos[0] - self.rect.center[0]) if abs(pos[0] - self.rect.center[0]) > 0 else 0.001, \
+                           abs(pos[1] - self.rect.center[1])
+            velocity_x = self.move_speed / math.sqrt(1 + (leg_y / leg_x))
+            velocity_y = velocity_x * (leg_y / leg_x)
+            if 0 <= leg_x < 10:
+                velocity_x = 0
+            if 0 <= leg_y < 10:
+                velocity_y = 0
+            if velocity_x > 3:
+                velocity_x = 3
+            if velocity_y > 3:
+                velocity_y = 3
+
+            if pos[0] >= self.rect.center[0] and pos[1] <= self.rect.center[1]:
+                to_point = pygame.math.Vector2((velocity_x, -velocity_y))
+            elif pos[0] >= self.rect.center[0] and pos[1] >= self.rect.center[1]:
+                to_point = pygame.math.Vector2((velocity_x, velocity_y))
+            elif pos[0] <= self.rect.center[0] and pos[1] <= self.rect.center[1]:
+                to_point = pygame.math.Vector2((-velocity_x, -velocity_y))
+            elif pos[0] <= self.rect.center[0] and pos[1] >= self.rect.center[1]:
+                to_point = pygame.math.Vector2((-velocity_x, velocity_y))
+            else:
+                to_point = pygame.math.Vector2()
+            self.rect.center += to_point
+
+            self.avoid()
+            self.collide()
+
+        except ValueError:
+            pass
+
+        finally:
+            if isinstance(self, unit_sprites.CrusaderWorkerSprite) and abs(pos[0] - self.rect.center[0]) < 30 \
+                    and abs(pos[1] - self.rect.center[1]) < 30:
+                return 1
+            elif isinstance(self, units.CrusaderSoldier) and abs(pos[0] - self.rect.center[0]) < self.attack_range \
+                    and abs(pos[1] - self.rect.center[1]) < self.attack_range:
+                return 1
+            return 0
+
+
+class Obstacle(PhysicObject):
+
+    def __init__(self, game):
+        super().__init__(game)
